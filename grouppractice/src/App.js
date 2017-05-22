57a776ca44aa4a7157f46be2c24a6fd491a4cb81
zahra abdallah <zkon360@gmail.com>
2017-05-22 12:17:56 -0400

    Created an Aside.jsx

--- grouppractice/src/App.js
+++ grouppractice/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Footer from './components/Footer'
 import Main from './components/Main';
+import Aside from '.components/Aside';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Aside value="This is an aside"/>
         <Footer />
         <Main/>
       </div>
---
+++ grouppractice/src/components/Aside.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Aside extends Component{
+ render(){
+   return(
+   <Aside value={this.props.value} />
+   );
+ }
+}
+
+export default Aside;
