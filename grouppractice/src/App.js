550699d8a9ee0ec3571a09e00e5865ace558d8c6
zahra abdallah <zkon360@gmail.com>
2017-05-22 12:39:55 -0400

    Navbar

--- grouppractice/src/App.js
+++ grouppractice/src/App.js
@@ -5,13 +5,16 @@ import Main from './components/Main';
 
 //import components
 import Header from './components/Header.jsx';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Nav/>
         <Main/>
+        <Aside/>
         <Footer />   
       </div>
     );
---
+++ grouppractice/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Nav extends Component{
+return(){
+  render(
+    <ul className="Nav">
+      <li> D</li>
+       <li>J</li>
+        <li>Z </li>
+    </ul>
+
+    );
+  }
+
+}
+export default Nav;
